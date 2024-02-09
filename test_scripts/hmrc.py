f14fe98b765ef9f11aae41e4c891f25b0aa74319
Daniel Seichter <daniel.seichter@erpware.de>
2024-02-09 22:04:20 +0100

    sample for hmrc and vies

--- test_scripts/hmrc.py
+++ test_scripts/hmrc.py
@@ -2,3 +2,27 @@ url = 'https://api.service.hmrc.gov.uk/organisations/vat/check-vat-number/lookup
 
 # example: without GB
 # https://api.service.hmrc.gov.uk/organisations/vat/check-vat-number/lookup/1234567890
+
+import urllib3
+from xml.dom import minidom
+import json
+
+http = urllib3.PoolManager()
+
+URL = 'https://api.service.hmrc.gov.uk/organisations/vat/check-vat-number/lookup/'
+OWNVAT=''
+FOREIGNVAT='GB243609761'
+COMPANY='Übungsfirma'
+TOWN=''
+ZIP=''
+STREET=''
+
+try:
+    resp = http.request("GET", URL + FOREIGNVAT[2:])
+    print(resp.status, resp.data)
+
+    # example response:
+    # {"target":{"name":"DEUTSCHE BANK AG LONDON","vatNumber":"243609761","address":{"line1":"21 MOORFIELDS","line2":"LONDON","postcode":"EC2Y 9DB","countryCode":"GB"}},"processingDate":"2024-02-09T20:30:07+00:00"}'
+
+except Exception as e:
+    print(repr(e))
--- test_scripts/vies.py
+++ test_scripts/vies.py
@@ -1,8 +1,65 @@
 url = 'https://ec.europa.eu/taxation_customs/vies/services/checkVatService'
 
-#   postbody := '<Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">' + '<Body xmlns="http://schemas.xmlsoap.org/soap/envelope/">' +
-#     '<checkVatApprox xmlns="urn:ec.europa.eu:taxud:vies:services:checkVat:types">' + '<countryCode>' + Copy(helper_normalizevat_request(importdata[importid].foreignvat), 0, 2) + '</countryCode>' +
-#     '<vatNumber>' + Copy(helper_normalizevat_request(importdata[importid].foreignvat), 3) + '</vatNumber>' +
 
-#     '<requesterCountryCode>' + Copy(helper_normalizevat_request(importdata[importid].ownvat), 0, 2) + '</requesterCountryCode>' + '<requesterVatNumber>' +
-#     Copy(helper_normalizevat_request(importdata[importid].ownvat), 3) + '</requesterVatNumber>' + '</checkVatApprox>' + '</Body>' + '</Envelope>';
+import urllib3
+from xml.dom import minidom
+import json
+
+http = urllib3.PoolManager()
+
+URL = 'https://ec.europa.eu/taxation_customs/vies/services/checkVatService'
+OWNVAT='DE323410633'
+FOREIGNVAT='IT01739710307'
+COMPANY=''
+TOWN=''
+ZIP=''
+STREET=''
+
+payload = f"""<Envelope xmlns=\"http://schemas.xmlsoap.org/soap/envelope/\">
+                <Body xmlns=\"http://schemas.xmlsoap.org/soap/envelope/\">
+                  <checkVatApprox xmlns=\"urn:ec.europa.eu:taxud:vies:services:checkVat:types\">
+                     <countryCode>{FOREIGNVAT[:2]}</countryCode>
+                     <vatNumber>{FOREIGNVAT[2:]}</vatNumber>
+                     <requesterCountryCode>{OWNVAT[:2]}</requesterCountryCode>
+                     <requesterVatNumber>{OWNVAT[2:]}</requesterVatNumber>
+                  </checkVatApprox>
+                </Body>
+               </Envelope>"""
+
+#print(payload)
+headers= {
+    'Content-Type': 'text/xml; charset=utf-8'
+}
+
+try:
+    resp = http.request("POST", URL, headers=headers, body=payload)
+    #print(resp.status, resp.data)
+
+    # example response:
+    # <env:Envelope xmlns:env="http://schemas.xmlsoap.org/soap/envelope/"><env:Header/><env:Body><ns2:checkVatApproxResponse xmlns:ns2="urn:ec.europa.eu:taxud:vies:services:checkVat:types"><ns2:countryCode>IT</ns2:countryCode><ns2:vatNumber>01739710307</ns2:vatNumber><ns2:requestDate>2024-02-09+01:00</ns2:requestDate><ns2:valid>false</ns2:valid><ns2:traderName></ns2:traderName><ns2:traderCompanyType>---</ns2:traderCompanyType><ns2:traderAddress></ns2:traderAddress><ns2:requestIdentifier></ns2:requestIdentifier></ns2:checkVatApproxResponse></env:Body></env:Envelope>'
+    dom = minidom.parseString(resp.data)
+    node = dom.documentElement
+
+    result = {}
+    try:
+        result['traderName'] = node.getElementsByTagName('ns2:traderName')[0].childNodes[0].nodeValue
+    except:
+        result['traderName'] = None
+    try:
+        result['traderAddress'] = node.getElementsByTagName('ns2:traderAddress')[0].childNodes[0].nodeValue
+    except:
+        result['traderAddress'] = None
+    try:
+        result['valid'] = node.getElementsByTagName('ns2:valid')[0].childNodes[0].nodeValue
+    except:
+        result['valid'] = None
+    try:
+        result['requestDate'] = node.getElementsByTagName('ns2:requestDate')[0].childNodes[0].nodeValue
+    except:
+        result['requestDate'] = None
+
+    print(result)
+
+    
+except Exception as e:
+    print(repr(e))
