f93ab51e4a52fde4949d97c8afc78cb38effb4c9
Daniel Seichter <daniel.seichter@erpware.de>
2024-07-18 21:53:43 +0200

    #37 Add GPL 3.0 information

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,3 +1,18 @@
+# Copyright (c) 2024 Daniel Seichter
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 # Example DNS record using Route53.
 # Route53 is not specifically required; any DNS host can be used.
 resource "aws_route53_record" "api_gateway_domain_name" {
